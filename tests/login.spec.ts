24482f89154fb28c9cea157ddae1f7eab074cb63
TakvorN <t.k.nahabedyan@gmail.com>
2026-06-18 06:18:38 +0300

    test: add locked out user login validation

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -9,3 +9,15 @@ test('standard user can log in', async ({ page }) => {
 
   await expect(page).toHaveURL(/inventory\.html/);
 });
+
+
+test('locked out user cannot log in', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+
+  await page.locator('[data-test="username"]').fill('locked_out_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+
+  await expect(page.locator('[data-test="error"]')).toBeVisible();
+  await expect(page.locator('[data-test="error"]')).toContainText('locked out');
+});
